ff86fbbc2dfdac08069b82afe01f754b6042b8db
talita <talitaramosrd@gmail.com>
2024-12-18 15:25:15 -0300

    feat(ProdutoDao): Adicionar função para atualizar produto

--- app/src/main/java/com/example/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/com/example/orgs/database/dao/ProdutoDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.orgs.model.Produto
 
 @Dao
@@ -17,4 +18,7 @@ interface ProdutoDao {
 
     @Delete
     fun remove(produto: Produto)
+
+    @Update
+    fun update(produto: Produto)
 }
